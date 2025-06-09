16a366643d989092f3e46410ce05b37c585e6aa1
Joshua Bassett <hello@joshbassett.info>
2025-06-09 21:21:48 +1000

    wip

--- hdl/top.v
+++ hdl/top.v
@@ -27,8 +27,8 @@ module top (
   wire [31:0] work_ram_dout;
   reg         char_ram_ready;
   wire [31:0] char_ram_dout;
-  reg         uart_ready;
-  wire [ 7:0] uart_dout;
+  reg         uart_tx_active;
+  // wire [ 7:0] uart_dout;
 
   // reset
   always @(posedge clk_25mhz) reset_cnt <= reset_cnt + !rst_n;
@@ -53,15 +53,15 @@ module top (
     rom_ready      <= rom_cs;
     work_ram_ready <= work_ram_cs;
     char_ram_ready <= char_ram_cs;
-    uart_ready     <= uart_cs;
   end
+
+  wire uart_ready = uart_cs && !uart_tx_active;
 
   // decode CPU memory ready signal
   assign cpu_mem_ready = uart_ready || led_cs || char_ram_ready || work_ram_ready || rom_ready;
 
   // decode CPU read data bus
   assign cpu_mem_rdata =
-    uart_cs ? {24'h0, uart_dout} :
     led_cs ? {24'h0, led} :
     char_ram_cs ? char_ram_dout :
     work_ram_cs ? work_ram_dout :
@@ -122,32 +122,42 @@ module top (
   );
 
   // UART
-  reg uart_e_clk;
-  reg uart_baud_clk;
-  reg [7:0] baud_cnt = 0;
+  // reg uart_e_clk;
+  // reg uart_baud_clk;
+  // reg [7:0] baud_cnt = 0;
+  //
+  // always @(posedge clk_25mhz) begin
+  //   uart_e_clk <= !uart_e_clk;
+  //   uart_baud_clk <= baud_cnt > 81;
+  //   baud_cnt <= baud_cnt + 1;
+  //   if (baud_cnt > 162) baud_cnt <= 0;
+  // end
+  //
+  // acia uart (
+  //     .clk(clk_25mhz),
+  //     .reset(!rst_n),
+  //     .cs(uart_cs),
+  //     .e_clk(uart_e_clk),
+  //     .rw_n(!cpu_mem_wstrb[0]),
+  //     .rs(cpu_mem_addr[2]),
+  //     .data_in(cpu_mem_wdata[7:0]),
+  //     .data_out(uart_dout),
+  //     .txclk(uart_baud_clk),
+  //     .rxclk(uart_baud_clk),
+  //     .txdata(ftdi_rxd),
+  //     .rxdata(ftdi_txd),
+  //     .cts_n(0),
+  //     .dcd_n(0)
+  // );
 
-  always @(posedge clk_25mhz) begin
-    uart_e_clk <= !uart_e_clk;
-    uart_baud_clk <= baud_cnt > 81;
-    baud_cnt <= baud_cnt + 1;
-    if (baud_cnt > 162) baud_cnt <= 0;
-  end
-
-  acia uart (
-      .clk(clk_25mhz),
-      .reset(!rst_n),
-      .cs(uart_cs),
-      .e_clk(uart_e_clk),
-      .rw_n(!cpu_mem_wstrb[0]),
-      .rs(cpu_mem_addr[2]),
-      .data_in(cpu_mem_wdata[7:0]),
-      .data_out(uart_dout),
-      .txclk(uart_baud_clk),
-      .rxclk(uart_baud_clk),
-      .txdata(ftdi_rxd),
-      .rxdata(ftdi_txd),
-      .cts_n(0),
-      .dcd_n(0)
+  uart_tx #(
+      .CLKS_PER_BIT(2604)
+  ) uart (
+      .i_Clock(clk_25mhz),
+      .i_Tx_DV(uart_cs ? cpu_mem_wstrb[0] : 0),
+      .i_Tx_Byte(cpu_mem_wdata[7:0]),
+      .o_Tx_Active(uart_tx_active),
+      .o_Tx_Serial(ftdi_rxd)
   );
 
 endmodule
--- rom/serial.c
+++ rom/serial.c
@@ -3,8 +3,7 @@
 #define RDRF 0
 #define TDRE 1
 
-volatile uint8_t *UART_CTRL = (uint8_t *)0x4000;
-volatile uint8_t *UART_DATA = (uint8_t *)0x4004;
+volatile uint8_t *UART_DATA = (uint8_t *)0x4000;
 
 void delay(uint32_t d) {
   for (uint32_t i = 0; i < d; i++) {
@@ -12,17 +11,8 @@ void delay(uint32_t d) {
   }
 }
 
-void serial_init(void) {
-  *UART_CTRL = 3; // reset UART
-  delay(10);
-  *UART_CTRL = 0x95; // RTS enabled 9600
-}
-
 void cout(char *a) {
   for (; *a != 0; a++) {
-    while ((*UART_CTRL & (1 << TDRE)) == 0) {
-      // wait
-    }
     *UART_DATA = *a;
   }
 }
@@ -30,8 +20,6 @@ void cout(char *a) {
 int __attribute__((noreturn)) main() {
   char *line = "HELLO WORLD!\n\r";
   char c = 0;
-
-  serial_init();
 
   while (1) {
     line[0] = '0' + (7 & c++);

172ab4c555d81dc10fbcc20680043e93c818d1e8
Joshua Bassett <hello@joshbassett.info>
2025-06-21 22:01:24 +1000

    Print hex value

--- rom/display.c
+++ rom/display.c
@@ -4,6 +4,8 @@ volatile uint16_t *CHAR_RAM = (uint16_t *)0x2000;
 volatile uint16_t *PARAM_RAM = (uint16_t *)0x2800;
 volatile uint8_t *LED = (uint8_t *)0x3000;
 
+const char digit[] = "0123456789ABCDEF";
+
 // Address offset of the first printable ASCII character
 #define ASCII_OFFSET 0x20
 
@@ -25,7 +27,7 @@ void clear_text() {
   }
 }
 
-void write_text(char *s, uint16_t flags, uint8_t col, uint8_t row) {
+void write_text(const char *s, uint16_t flags, uint8_t col, uint8_t row) {
   uint8_t i = (row << 5) + col;
   while (*s) {
     char c = *s++ - ASCII_OFFSET;
@@ -33,20 +35,29 @@ void write_text(char *s, uint16_t flags, uint8_t col, uint8_t row) {
   }
 }
 
+void write_uint16(uint16_t n, uint16_t flags, uint8_t col, uint8_t row) {
+  uint8_t j = (row << 5) + col;
+  CHAR_RAM[j++] = '$' - ASCII_OFFSET;
+  for (int i = 3; i >= 0; i--) {
+    uint8_t k = n >> (i << 2);
+    char s = digit[k & 0x0F];
+    char c = s - ASCII_OFFSET;
+    CHAR_RAM[j++] = (flags << 12) | c;
+  }
+}
+
 int __attribute__((noreturn)) main() {
   clear_params();
   clear_text();
 
-  *PARAM_RAM = 0x00FF;
-  *LED = *PARAM_RAM;
+  *PARAM_RAM = 0x3FFF;
 
   write_text("FILTER (1/2)                ++++", TEXT_INVERT, 0, 0);
   write_text("FREQ    RES     ENV     MODE    ", TEXT_NORMAL, 0, 2);
   write_text("1.00    0.01    0.00    LOW PASS", TEXT_NORMAL, 0, 3);
-  write_text("----    ----    ----    ----    ", TEXT_NORMAL, 0, 4);
+  write_uint16(*PARAM_RAM, TEXT_NORMAL, 0, 3);
   write_text("ATK     DEC     SUS     REL     ", TEXT_NORMAL, 0, 5);
   write_text("0.64    1.74    0.34    0.44    ", TEXT_NORMAL, 0, 6);
-  write_text("----    ----    ----    ----    ", TEXT_NORMAL, 0, 7);
 
   while (1) {
   }

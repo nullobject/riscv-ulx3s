de40525c5305343e9a6d3ed64588a0f537dbcdd4
Joshua Bassett <hello@joshbassett.info>
2025-07-03 20:44:12 +1000

    Use stdio in serial example

--- examples/serial.c
+++ examples/serial.c
@@ -1,21 +1,14 @@
-#include <stdint.h>
+#include <stdio.h>
 
 #include "../lib/hal.h"
 
 void irq() { *LED = *UART0; }
 
-void cout(char *a) {
-  while (*a) {
-    *UART0 = *a++;
-  }
-}
-
 int __attribute__((noreturn)) main() {
-  char *line = "HELLO WORLD!\n\r";
   *LED = 0x00;
 
   while (1) {
-    cout(line);
+    printf("Hello World!\n");
     delay(65535);
   }
 }

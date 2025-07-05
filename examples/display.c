e40d69b1ff1f68873581ecbb61a0aa15ccacb691
Joshua Bassett <hello@joshbassett.info>
2025-07-05 17:45:58 +1000

    Update display example to print MIDI messages

--- examples/display.c
+++ examples/display.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 
+#include "../lib/fifo.h"
 #include "../lib/hal.h"
 #include "../lib/midi.h"
 
@@ -11,23 +12,25 @@ const char BOT_BAR[] = {204, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205,
                         205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205,
                         205, 205, 205, 205, 205, 205, 205, 205, 205, 220, 0};
 
-midi_parser *p = {0};
+midi_parser p = {0};
+fifo q;
 
+// TODO: Check which UART1 caused the interrupt
 void irq() {
-  midi_message m = midi_parse(p, *UART1);
-  if (m.status) {
-    *LED = ~*LED;
+  midi_message msg = midi_parse(&p, *UART1);
+  if (msg.status) {
+    fifo_write(&q, &msg);
   }
 }
 
 int __attribute__((noreturn)) main() {
+  fifo_init(&q, 8, sizeof(midi_message));
   clear_text();
 
   write_text(TOP_BAR, TEXT_NORMAL, 0, 0);
   write_text(BOT_BAR, TEXT_NORMAL, 0, 7);
 
-  write_text("FREQ    RES     ENV     MODE    ", TEXT_NORMAL, 0, 2);
-  write_text("ATK     DEC     SUS     REL     ", TEXT_NORMAL, 0, 5);
+  write_text("        VALUE1  VALUE2          ", TEXT_NORMAL, 0, 2);
 
   ENCODERS[0] = *PRNG;
   ENCODERS[1] = *PRNG;
@@ -39,13 +42,37 @@ int __attribute__((noreturn)) main() {
   ENCODERS[7] = *PRNG;
 
   while (1) {
-    write_int16(ENCODERS[0] - 32768, TEXT_NORMAL, 0, 3);
-    write_uint16(ENCODERS[1], TEXT_NORMAL, 8, 3);
-    write_uint16(ENCODERS[2], TEXT_NORMAL, 16, 3);
-    write_uint16(ENCODERS[3], TEXT_NORMAL, 24, 3);
-    write_uint16(ENCODERS[4], TEXT_NORMAL, 0, 6);
-    write_uint16(ENCODERS[5], TEXT_NORMAL, 8, 6);
-    write_uint16(ENCODERS[6], TEXT_NORMAL, 16, 6);
-    write_uint16(ENCODERS[7], TEXT_NORMAL, 24, 6);
+    midi_message *c = fifo_read(&q);
+    if (c) {
+      if (c->status == MIDI_NOTE_OFF) {
+        write_text("NOTEOFF ", TEXT_NORMAL, 0, 2);
+        write_uint16(c->data[0], TEXT_NORMAL, 8, 3);
+        write_uint16(c->data[1], TEXT_NORMAL, 16, 3);
+      } else if (c->status == MIDI_NOTE_ON) {
+        write_text("NOTEON  ", TEXT_NORMAL, 0, 2);
+        write_uint16(c->data[0], TEXT_NORMAL, 8, 3);
+        write_uint16(c->data[1], TEXT_NORMAL, 16, 3);
+      } else if (c->status == MIDI_POLY_PRESSURE) {
+        write_text("PPRESS  ", TEXT_NORMAL, 0, 2);
+        write_uint16(c->data[0], TEXT_NORMAL, 8, 3);
+        write_uint16(c->data[1], TEXT_NORMAL, 16, 3);
+      } else if (c->status == MIDI_CONTROL_CHANGE) {
+        write_text("CTRL    ", TEXT_NORMAL, 0, 2);
+        write_uint16(c->data[0], TEXT_NORMAL, 8, 3);
+        write_uint16(c->data[1], TEXT_NORMAL, 16, 3);
+      } else if (c->status == MIDI_PROGRAM_CHANGE) {
+        write_text("PROG    ", TEXT_NORMAL, 0, 2);
+        write_uint16(c->data[0], TEXT_NORMAL, 8, 3);
+      } else if (c->status == MIDI_CHANNEL_PRESSURE) {
+        write_text("PRESS   ", TEXT_NORMAL, 0, 2);
+        write_uint16(c->data[0], TEXT_NORMAL, 8, 3);
+      } else if (c->status == MIDI_PITCH_WHEEL) {
+        write_text("BEND    ", TEXT_NORMAL, 0, 2);
+        write_uint16(c->data[0], TEXT_NORMAL, 8, 3);
+        write_uint16(c->data[1], TEXT_NORMAL, 16, 3);
+      } else if (c->status != 0xB0) {
+        write_text("ERROR", TEXT_NORMAL, 0, 2);
+      }
+    }
   }
 }
